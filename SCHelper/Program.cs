44a3617f9db73c4aa05ebc5f768b6ea417a5c50b
SSkeptix <sskeptix@gmail.com>
2021-02-07 13:45:31 +0200

    Added configuration sample

--- SCHelper/Program.cs
+++ SCHelper/Program.cs
@@ -3,31 +3,55 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SCHelper.Services;
 using SCHelper.Services.Impl;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SCHelper
 {
     public class Program
     {
+        private const string DefaultConfigFileName = "appsettings.json";
+
         public static Task Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile($"appsettings.json", false, true)
-                .Build();
+            var configuration = GetConfiguration();
+            if (configuration == null)
+                return Task.CompletedTask;
 
-            using (var serviceProvider = new ServiceCollection()
-                .AddSingleton<IDataProvider, DataProvider>()
-                .AddSingleton<Startup, Startup>()
-                .AddSingleton<IConfiguration>(configuration)
-                .AddLogging(loggingBuilder => loggingBuilder
-                    .ClearProviders()
-                    .AddConsole()
-                    .SetMinimumLevel(LogLevel.Trace))
-                .BuildServiceProvider())
+            using (var serviceProvider = GetServiceProvider(configuration))
             {
                 var startup = serviceProvider.GetService<Startup>();
                 return startup.Execute();
             };
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            if (File.Exists(DefaultConfigFileName))
+            {
+                return new ConfigurationBuilder()
+                    .AddJsonFile(DefaultConfigFileName, false, true)
+                    .Build();
+            }
+            else
+            {
+                // Create sample file
+                // Create ReadMe.txt
+                return null;
+            }            
+        }
+
+        private static ServiceProvider GetServiceProvider(IConfiguration configuration)
+        {
+            return new ServiceCollection()
+                .AddSingleton<IDataProvider, DataProvider>()
+                .AddSingleton<Startup, Startup>()
+                .AddSingleton(configuration)
+                .AddLogging(loggingBuilder => loggingBuilder
+                    .ClearProviders()
+                    .AddConsole()
+                    .SetMinimumLevel(LogLevel.Trace))
+                .BuildServiceProvider();
         }
     }
 }
