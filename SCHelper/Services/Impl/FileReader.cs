ea4c36f3cda563beee07fc02c30ae29fea3d44b4
SSkeptix <sskeptix@gmail.com>
2021-02-28 12:50:06 +0200

    Fixed data parsing issue (because of user culture)

--- SCHelper/Services/Impl/FileReader.cs
+++ SCHelper/Services/Impl/FileReader.cs
@@ -11,7 +11,11 @@ namespace SCHelper.Services.Impl
         public T[] Read<T>(string filePath)
             where T : class, new()
         {
-            var configuration = new CsvConfiguration(CultureInfo.CurrentCulture) { MissingFieldFound = null };
+            var configuration = new CsvConfiguration(new CultureInfo("en-US", false))
+            {
+                MissingFieldFound = null,
+                Delimiter = ",",              
+            };
             using (var streamReader = new StreamReader(filePath))
             using (var csvReader = new CsvReader(streamReader, configuration))
             {
